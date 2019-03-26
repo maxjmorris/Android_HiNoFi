20eac3ba9011f34fc56d63d4dacf5e61a9ccf10c
Max Morris <p15213515@my365.dmu.ac.uk>
2019-03-26 21:28:49 +0000

    Cleaning up redundant data and some edits to the Help Page

--- app/src/main/java/com/example/android_hinofi_prototype/activities/HomeActivity.java
+++ app/src/main/java/com/example/android_hinofi_prototype/activities/HomeActivity.java
@@ -2,37 +2,17 @@ package com.example.android_hinofi_prototype.activities;
 
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
-
 import com.example.android_hinofi_prototype.R;
 import com.example.android_hinofi_prototype.adapters.FragmentPageAdapter;
 
 public class HomeActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
 
     /**
      * The {@link ViewPager} that will host the section contents.
--- app/src/main/java/com/example/android_hinofi_prototype/activities/LoginActivity.java
+++ app/src/main/java/com/example/android_hinofi_prototype/activities/LoginActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-
 import com.example.android_hinofi_prototype.R;
 import com.example.android_hinofi_prototype.adapters.DatabaseAdapter;
 
